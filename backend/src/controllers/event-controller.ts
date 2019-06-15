ec683376b7a1fc88cad59a725b3d2df897b4d2b0
Mandana <mandanakhademi88@gmail.com>
2019-06-15 21:18:09 +0100

    add getAllCareRecipiets function to event controller

--- backend/src/controllers/event-controller.ts
+++ backend/src/controllers/event-controller.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import DatabaseService from '../services/database-service';
 
 class EventController {
@@ -5,6 +6,16 @@ class EventController {
 
     constructor(dbService: DatabaseService) {
         EventController.dbService = dbService;
+    }
+
+    public getAllCareRecipiets(_: Request, res: Response) {
+        EventController.dbService.getAllCareRecipiets().then(result => {
+            const flatResult = new Array();
+            result.forEach((element: { care_recipient_id: any; }) => {
+                flatResult.push(element.care_recipient_id);
+            });
+            res.json(flatResult);
+        });
     }
 }
 
