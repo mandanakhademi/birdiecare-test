d2655fd22ad6b8703c61a5a6afe96455feb945e1
Mandana <mandanakhademi88@gmail.com>
2019-06-15 21:20:26 +0100

    add body parser to application

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,7 +1,11 @@
+import * as bodyParser from "body-parser"
 import * as express from "express";
-import {pingController} from "./controllers/ping";
+import { pingController } from "./controllers/ping";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(pingController);
 
