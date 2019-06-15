e360b578329a1b32bc36900019d8bb4bfbf597fc
Mandana <mandanakhademi88@gmail.com>
2019-06-15 21:35:33 +0100

    add careRecipiets route

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,11 +1,19 @@
 import * as bodyParser from "body-parser"
 import * as express from "express";
+import EventController from "./controllers/event-controller";
 import { pingController } from "./controllers/ping";
+import DatabaseService from "./services/database-service";
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const databaseService = new DatabaseService();
+const eventController = new EventController(databaseService);
+
+app.use('/careRecipiets', eventController.getAllCareRecipiets);
 
 app.use(pingController);
 
