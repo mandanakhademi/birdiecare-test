36642b6bea54fc1813779515e11cf5699a5f1ca2
Mandana <mandanakhademi88@gmail.com>
2019-06-17 01:16:08 +0100

    add required packages to front end

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import store from '@App/store';
-import { Provider } from 'react-redux';
-import App from '@App/components/app/App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import store from "@App/store";
+import { Provider } from "react-redux";
+import App from "@App/components/app/App";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
